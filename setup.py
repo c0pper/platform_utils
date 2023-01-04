8306aa47821eb4fcaffe80acc524c8e806810de5
93simon <93simonster@gmail.com>
2023-01-04 13:01:43 +0100

    minor fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='platform_utils_eai',
     packages=find_packages(include=['platform_utils_eai']),
-    version='0.1.0',
+    version='0.1.1',
     description='Collection of functions and utilities to create annotated libraries to be uploaded on EAI Platform',
     author='Simone Martin Marotta',
     install_requires=[],
